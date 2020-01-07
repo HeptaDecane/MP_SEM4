e614965374493a2f8387e7fd939507622de556fd
NearLawiet <sooraj1999vs@gmail.com>
2020-01-08 00:41:58 +0530

    Completed Non-OverLapping

--- a02A.asm
+++ a02A.asm
@@ -1,3 +1,4 @@
+
 %macro print 2
 	mov rax,01
 	mov rdi,01
@@ -13,18 +14,40 @@
 	mov rdx,%2
 	syscall
 %endmacro
+;_______________________________________________________________________________
 
 section .data
+	title: db "NON-OVERLAPPED BLOCK TRANSFER"
+	lenTitle: equ $-title
+
+	option1: db "Press 1: Block Transfer Without String Instructions"
+	lenOption1: equ $-option1
+
+	option2: db "Press 2: Block Transfer With String Instructions"
+	lenOption2: equ $-option2
+
+	option3: db "Press 0: Exit"
+	lenOption3: equ $-option3
+
+	request: db "Enter Choice: "
+	lenRequest: equ $-request
+
+	message1: db "Block Transfer WITHOUT String Instructions:"
+	lenMessage1: equ $-message1
+
+	message2: db "Block Transfer WITH String Instructions:"
+	lenMessage2: equ $-message2
+
 	srcBlkBfr: db "Source Block Before Transfer: "
 	lenSrcBlkBfr: equ $-srcBlkBfr
 
-	desBlkBfr: db "Dest'n Block Before Transfer: "
+	desBlkBfr: db "Dest^n Block Before Transfer: "
 	lenDesBlkBfr: equ $-desBlkBfr
 
 	srcBlkAftr: db "Source Block After Transfer: "
 	lenSrcBlkAftr: equ $-srcBlkAftr
 
-	desBlkAftr: db "Dest'n Block After Transfer: "
+	desBlkAftr: db "Dest^n Block After Transfer: "
 	lenDesBlkAftr: equ $-desBlkAftr
 
 	space: db " "
@@ -35,14 +58,13 @@ section .data
 
 	desBlk: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 	n2: equ 16
+;_______________________________________________________________________________
 
 section .bss
-	optionbuff resb 02
-	dispbuff resb 02
-	char_answer resb 16
-	digitSpace resb 100
-	digitSpacePos resb 8
-
+	choice: resb 01
+	digitSpace: resb 100
+	digitSpacePos: resb 8
+;_______________________________________________________________________________
 
 section .text
 	global _start:
@@ -55,8 +77,36 @@ section .text
 		call _disDesBlk
 		print newLine,1
 
+		menu:
+		print newLine,1
+		print title,lenTitle
+		print newLine,1
+		print option1,lenOption1
+		print newLine,1
+		print option2,lenOption2
+		print newLine,1
+		print option3,lenOption3
 		print newLine,1
 
+		print request,lenRequest
+		read choice,2		;* 2 bytes for read statement to execute each time
+		print newLine,1
+
+		cmp byte[choice],31h
+		je label1
+
+		cmp byte[choice],32h
+		je label2
+
+		exit:
+		mov rax,60
+		mov rdi,00
+		syscall
+
+
+		label1:	;* Without String Instructions
+		print message1,lenMessage1
+		print newLine,1
 		call _tranferLogic
 
 		print srcBlkAftr,lenSrcBlkAftr
@@ -67,35 +117,34 @@ section .text
 		call _disDesBlk
 		print newLine,1
 
-		mov rax,60
-		mov rdi,00
-		syscall
+		jmp menu
 
-;_____________________________________________________________
+
+		label2:		;* Without String Instructions
+		print message2,lenMessage2
+		print newLine,1
+
+		mov rsi,srcBlk
+		mov rdi,desBlk
+		mov rcx,16
+
+		cld
+		rep movsb
+
+		print srcBlkAftr,lenSrcBlkAftr
+		call _disSrcBlk
+		print newLine,1
+
+		print desBlkAftr,lenDesBlkAftr
+		call _disDesBlk
+		print newLine,1
+
+		jmp menu
+;_______________________________________________________________________________
 
 _disSrcBlk:
 	mov rsi,srcBlk
 	mov rcx,n1
-
-	beginLoop2:
-	mov al,byte[rsi]
-	push rsi
-	push rcx
-	call _printRAX
-	print space,1
-	pop rcx
-	pop rsi
-
-	update2:
-	inc rsi
-	dec rcx
-	jnz beginLoop1
-	ret
-
-
-_disDesBlk:
-	mov rsi,desBlk
-	mov rcx,n2
 
 	beginLoop1:
 	mov al,byte[rsi]
@@ -111,7 +160,27 @@ _disDesBlk:
 	dec rcx
 	jnz beginLoop1
 	ret
+;_______________________________________________________________________________
 
+_disDesBlk:
+	mov rsi,desBlk
+	mov rcx,n2
+
+	beginLoop2:
+	mov al,byte[rsi]
+	push rsi
+	push rcx
+	call _printRAX
+	print space,1
+	pop rcx
+	pop rsi
+
+	update2:
+	inc rsi
+	dec rcx
+	jnz beginLoop1
+	ret
+;_______________________________________________________________________________
 
 _tranferLogic:
 	mov rsi,srcBlk
@@ -128,9 +197,9 @@ _tranferLogic:
 	dec rcx
 	jnz beginLoop3
 	ret
+;_______________________________________________________________________________
 
-
-_printRAX:
+_printRAX:		;Hex to ASCII
 	mov rcx, digitSpace
 	mov rbx, 0
 	mov [rcx], rbx
@@ -170,3 +239,4 @@ _printRAXLoop2:
 	jge _printRAXLoop2
 
 	ret
+;_______________________________________________________________________________
